b25a2f164adf5baa789b71c4ba23878a519ae614
seunghun-5945 <tmdgns5945@oasis.inje.ac.kr>
2024-12-28 21:28:26 +0900

    feat: add file and edit index.css

--- pnn-web-front/src/App.jsx
+++ pnn-web-front/src/App.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import styled from "styled-components";
+import GlobalNavigationBar from "./components/GlobalNavigationBar";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+`;
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-    </>
-  )
-}
+    <Container>
+      <GlobalNavigationBar />
+    </Container>
+  );
+};
 
-export default App
+export default App;
---
+++ pnn-web-front/src/components/GlobalNavigationBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 10%;
+  background-color: gray;
+
+  @media (min-width: 768px) {
+    height: 15%;
+    background-color: lightgray;
+  }
+`;
+
+const GlobalNavigationBar = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default GlobalNavigationBar;
