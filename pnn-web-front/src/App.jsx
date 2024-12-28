a13cf4d3ff95015a7ddace06c860dfec1bbc2aed
kjm9547 <woals8115@naver.com>
2024-12-28 21:33:12 +0900

    test commit

--- pnn-web-front/src/App.jsx
+++ pnn-web-front/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,8 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div>?????</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
