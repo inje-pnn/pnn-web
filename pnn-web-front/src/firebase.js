efcc0f48030b617b54d91cf2a24d58c2b439fa27
kjm9547 <woals8115@naver.com>
2024-12-30 23:35:17 +0900

    feat : mui, js-cookie,zustand 추가 및 firebase auth 설정 추가

--- pnn-web-front/src/firebase.js
+++ pnn-web-front/src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +14,11 @@ const firebaseConfig = {
   storageBucket: "pnn-web-4bad4.firebasestorage.app",
   messagingSenderId: "961986237709",
   appId: "1:961986237709:web:a1dd841e35a7a340700c44",
-  measurementId: "G-XF6SJJXFX8"
+  measurementId: "G-XF6SJJXFX8",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
