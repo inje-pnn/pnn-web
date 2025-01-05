281cc249199f1d86faaa44fa74e32897a7ba18a4
jihun8861 <ji55hun@oasis.inje.ac.kr>
2025-01-05 21:09:23 +0900

    feat: SharePage 추가

--- pnn-web-front/src/app/router/RootRoutes.jsx
+++ pnn-web-front/src/app/router/RootRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthPage } from "../../pages/auth/AuthPage";
 import { UserRegistPage } from "../../pages/auth/UserRegistPage";
+import { SharePage } from "../../pages/share/SharePage";
 import Layout from "../../widgets/layout/Layout";
 
 export const RootRoutes = () => {
   return (
     <Routes>
       <Route path="/auth" element={<Layout mainContent={<AuthPage />} />} />
+      <Route path="/share" element={<Layout mainContent={<SharePage />} />} />
       <Route
         path="/auth/regist"
         element={<Layout mainContent={<UserRegistPage />} />}
---
+++ pnn-web-front/src/pages/share/SharePage.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SharePage = () => {
+  return (
+    <Container>
+      공유 페이지
+    </Container>
+  );
+};
